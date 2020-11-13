69fa24db68058af2f3cd67c678867283c0df1fff
SlyStimpy <crowderdave641@gmail.com>
2020-11-12 22:49:03 -0500

    Added redirect functionality to home page

--- Home/views.py
+++ Home/views.py
@@ -48,5 +48,26 @@ class SessionDeleteView(DeleteView):
 
 
 def home(request):
+	if request.method == 'GET':
+		if request.GET['login-signup'] == 'login':
+			user_in = False
+			for i in User.objects.all():
+				if i.name_first == request.GET['f_name'] and i.name_last == request.GET['l_name'] and i.computingid == request.GET['user_id']:
+					user_in = True
+					break
+			if not user_in:
+				return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+			# Redirect back to last page if info provided isn't in the DB
+			# Make an ELSE statmemnt to pass through user id through to other pages
+		else:
+			user_in = False
+			for i in User.objects.all():
+				if i.computingid == request.GET['user_id']:
+					user_in = True
+					break
+			if user_in:
+				return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+			# Make ELSE statement to pass through a session var to the other pages
+			# Redirect back if userid already found in DB, otherwise set session var to user id
 	return render(request,'Home/home.html')
 
